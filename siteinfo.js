1988e065218f245cdac7f186e5d90f68969c10d9
Jake <68141675+ABCoins@users.noreply.github.com>
2021-05-13 10:48:30 +0800

    Add files via upload

---
+++ siteinfo.js
@@ -0,0 +1,11 @@
+var e = {
+    name: "we7_wxappdemo",
+    uniacid: "42",
+    acid: "42",
+    multiid: "0",
+    version: "1.0.0",
+    siteroot: "https://we.linknew.cn/app/index.php",
+    method_design: "3"
+};
+
+module.exports = e;
